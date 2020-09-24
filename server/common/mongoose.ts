ccf4059b603d57a03dc9772a9b1a624d3b8d1aa6
idoraz <idoraz84@gmail.com>
2020-09-25 00:10:48 +0300

    Update DB connection string to support atlas migration

--- server/common/mongoose.ts
+++ server/common/mongoose.ts
@@ -1,14 +1,17 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 export default class Mongoose {
-  connectionURI = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`;
+  connectionURI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+  // connectionURI = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`;
 
-  constructor() {
-  }
+  constructor() {}
 
   init() {
     //Set up default mongoose connection
-    mongoose.connect(this.connectionURI, { useFindAndModify: false, useNewUrlParser: true });
+    mongoose.connect(this.connectionURI, {
+      useFindAndModify: false,
+      useNewUrlParser: true,
+    });
 
     // Get Mongoose to use the global promise library
     mongoose.Promise = global.Promise;
@@ -17,6 +20,6 @@ export default class Mongoose {
     const db = mongoose.connection;
 
     //Bind connection to error event (to get notification of connection errors)
-    db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+    db.on("error", console.error.bind(console, "MongoDB connection error:"));
   }
 }
